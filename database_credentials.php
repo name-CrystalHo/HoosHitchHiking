589b3fce35ed90a5040d18eab99806d14ac71dc5
jtkim03 <jtk2rw@virginia.edu>
2021-10-24 20:09:04 -0400

    add: database credentials and HHControllere

---
+++ database_credentials.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = "localhost";
+$dbusername = "jtk2rw";
+$dbpasswd = "UQ6KlNzWK5Mk";
+$dbname = "jtk2rw"
+
+$clientID = '442159528554-f11452a77u4n3vi56msv8sff6b2t28bc.apps.googleusercontent.com';
+$clientSecret = '442159528554-f11452a77u4n3vi56msv8sff6b2t28bc.apps.googleusercontent.com';
+$redirectUri = 'http://localhost/signup.php';
+?>
